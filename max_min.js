d96e8dbfc9be3ed308b8c73c0583b86f5238e771
Keita Takahashi <takanenokei@gmail.com>
2026-04-08 13:44:52 +0900

    feat: add max/min solution

---
+++ max_min.js
@@ -0,0 +1,22 @@
+function main(input) {
+    const lines = input.split('\n');
+    const n = parseInt(lines[0]);
+    const numbers = lines[1].split(' ').map(Number);
+
+    let max = numbers[0];
+    let min = numbers[0];
+    for (let i = 1; i < n; i++) {
+        if (max < numbers[i]) {
+            max = numbers[i];
+        }
+
+        if (min > numbers[i]) {
+            min = numbers[i];
+        }
+    }
+
+    console.log(max);
+    console.log(min);
+}
+
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));

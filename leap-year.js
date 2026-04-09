f03f0c17b08eaa5752a9f092907944150a91a368
Keita Takahashi <takanenokei@gmail.com>
2026-04-10 01:56:25 +0900

    feat: add leap year solution

---
+++ leap-year.js
@@ -0,0 +1,17 @@
+function main(input) {
+    const lines = input.trim().split('\n');
+
+    const year = Number(lines[0]);
+
+    if (year % 400 === 0) {
+        console.log('Leap Year');
+    } else if (year % 100 === 0) {
+        console.log('Common Year');
+    } else if (year % 4 === 0) {
+        console.log('Leap Year');
+    } else {
+        console.log('Common Year');
+    }
+}
+
+main(require('fs').readFileSync('/dev/stdin', 'utf8'))

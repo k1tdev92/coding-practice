4b258368970614a65069235f8d45245eda6dbbb0
Keita Takahashi <takanenokei@gmail.com>
2026-04-11 09:49:53 +0900

    feat: add welcome-to-atcoder solution

---
+++ atcoder/abs/welcome-to-atcoder.js
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.trim().split('\n');
+
+const a = Number(lines[0]);
+const tmp = lines[1].split(' ');
+const b = Number(tmp[0]);
+const c = Number(tmp[1]);
+const s = lines[2];
+
+console.log(`${a + b + c} ${s}`);

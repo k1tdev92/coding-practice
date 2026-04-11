8d8f25d9537cffb09f800fd3c4c29ab93b964d1b
Keita Takahashi <takanenokei@gmail.com>
2026-04-12 01:47:01 +0900

    feat: add comma-separated-number solution

---
+++ paiza-levelup/c-rank/comma-separated-number.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.trim().split('\n');
+
+const n = lines[0];
+
+let result = '';
+for (let i = n.length - 1; i >= 0; i--) {
+    result = n[i] + result;
+    if ((n.length - i) % 3 === 0 && i !== 0) {
+        result = ',' + result;
+    }
+}
+
+console.log(result);

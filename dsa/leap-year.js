a8579c6b3553c41b532c7cb3808d219221896413
Keita Takahashi <takanenokei@gmail.com>
2026-04-17 23:58:43 +0900

    refactor: rewrite leap year using early rejection conditions

--- dsa/leap-year.js
+++ dsa/leap-year.js
@@ -3,12 +3,12 @@ const lines = input.trim().split('\n');
 
 const year = Number(lines[0]);
 
-if (year % 400 === 0) {
-  console.log('Leap Year');
-} else if (year % 100 === 0) {
+if (year % 4 !== 0) {
   console.log('Common Year');
-} else if (year % 4 === 0) {
+} else if (year % 100 !== 0) {
   console.log('Leap Year');
+} else if (year % 400 !== 0) {
+  console.log('Common Year');
 } else {
-  console.log('Common Year');
+  console.log('Leap Year');
 }
